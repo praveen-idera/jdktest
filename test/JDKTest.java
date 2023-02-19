ca1ea111ed051f3de893954d1025e0963cf245a9
praveen.rondi <praveen.rondi@idera.com>
2023-02-19 23:35:54 +0530

    no message

--- test/JDKTest.java
+++ test/JDKTest.java
@@ -8,4 +8,7 @@ public static void main(String args[])
  System.out.println("Java Virtual Machine implementation version : "+System.getProperty("java.vm.version"));
  System.out.println("Java Virtual Machine implementation vendor : "+System.getProperty("java.vm.vendor"));
  System.out.println("Java Virtual Machine implementation name : "+System.getProperty("java.vm.name"));
+  System.out.println("java.version : "+System.getProperty("java.version"));
+   System.out.println(" java.vendor.url : "+System.getProperty(" java.vendor.url"));
+    System.out.println("Java Virtual Machine implementation name : "+System.getProperty("java.vm.name"));
 }}  
